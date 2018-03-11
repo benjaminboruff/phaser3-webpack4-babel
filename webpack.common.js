b32bb9fb97c0c84b79ffd035a7143a4f8e38fd5a
Benjamin H Boruff <ben@boruff.me>
2018-03-10 21:12:12 -0500

    added eslint, cleaned up webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,28 @@
-const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
     module: {
         rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
-            }
-          },
-          {
-              test: [ /\.vert$/, /\.frag$/ ],
-              use: 'raw-loader'
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: [
+                    /\.vert$/,
+                    /\.frag$/
+                ],
+                use: 'raw-loader'
             }
         ]
-      },
-      plugins: [
-          new webpack.DefinePlugin({
-              'CANVAS_RENDERER': JSON.stringify(true),
-              'WEBGL_RENDERER': JSON.stringify(true)
-          })
-      ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'CANVAS_RENDERER': JSON.stringify(true),
+            'WEBGL_RENDERER': JSON.stringify(true)
+        })
+    ]
 }
